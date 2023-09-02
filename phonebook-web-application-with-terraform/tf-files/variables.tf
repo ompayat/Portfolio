262bfc4752fcea10786798a0c9519b6c967ad6e1
ompayat <ompayat@gmail.com>
2023-09-02 18:45:40 -0400

    phonebook application with terraform

---
+++ phonebook-web-application-with-terraform/tf-files/variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "ompayat"
+}
+
+variable "git-token" {
+  default = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+}
+
+variable "key-name" {
+  default = "Onur"
+}
+
+variable "hosted-zone" {
+  default = "devopson.me"
+}
